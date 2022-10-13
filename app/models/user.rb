b1c9841011510513f08e48cfba5e9bbbca6a58fb
Hiroyasu Tawara <zen_qp_mai@icloud.com>
2022-10-13 17:47:12 +0900

    Add: サインアップフォームの作成、ユーザーアイコン未選択時デフォルトの画像を表示できるように設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,12 @@
 class User < ApplicationRecord
-  mount_uploder :icon, iconUploader
+  mount_uploader :icon, IconUploader
 
   validates :name, presence: true, length: { maximum: 30 }
   validates :email, presence: true, length: { maximum: 255 }, uniqueness: true,
             format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
 
   before_validation { email.downcase! }
+
+  has_secure_password
+  validates :password, length: { minimum: 6 }
 end
--- app/uploaders/icon_uploader.rb
+++ app/uploaders/icon_uploader.rb
@@ -1,16 +1,20 @@
 class IconUploader < CarrierWave::Uploader::Base
-  
+  include CarrierWave::MiniMagick
   storage :file
   
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
+  def default_url(*args)
+    picture-user_default_icon.png
+  end
+
   def extension_whitelist
     %w(jpg jpeg gif png)
   end
   
-  include CarrierWave::MiniMagick
+  
   process resize_to_fill: [400, 400, "center"]
 
 end
