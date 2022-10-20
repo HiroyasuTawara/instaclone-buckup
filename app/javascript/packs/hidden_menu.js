a386e13b64ec08a4d9965c08c76a1776e5a17aae
Hiroyasu Tawara <zen_qp_mai@icloud.com>
2022-10-20 14:14:53 +0900

    Fix:一覧ページでモーダルウィンドウを開いた際、ページ内のすべてのウィンドウが展開していた不具合を修正

--- app/javascript/packs/hidden_menu.js
+++ app/javascript/packs/hidden_menu.js
@@ -1,18 +1,18 @@
 $(document).on('turbolinks:load',function(){
 	// 変数に要素を入れる
 	var open = $('.modal-open'),
-		close = $('.modal-close'),
+		close = $('.modal-close');
 		container = $('.modal-container');
 
 	//開くボタンをクリックしたらモーダルを表示する
 	open.on('click',function(){	
-		container.addClass('active');
+		$($(this).data('target')).addClass('active');
 		return false;
 	});
 
 	//閉じるボタンをクリックしたらモーダルを閉じる
 	close.on('click',function(){	
-		container.removeClass('active');
+		$($(this).data('target')).removeClass('active');
 	});
 
 	//モーダルの外側をクリックしたらモーダルを閉じる
