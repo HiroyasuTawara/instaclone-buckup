9a2ab77e87cbd735df585276bc0071714ee3de33
Hiroyasu Tawara <zen_qp_mai@icloud.com>
2022-10-21 13:53:08 +0900

    fix

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,7 @@ module Copygram
       g.helper false
     end
 
+    config.assets.initialize_on_precompile = false
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
